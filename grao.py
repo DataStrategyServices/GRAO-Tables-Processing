2c6c1111a9c63e6bb0650fd33784083a1498f074
Dimitar <dimitar.atanasov@indeavr.com>
2022-01-04 17:01:30 +0200

    fixed a bit of matching, need to fine-tune

--- grao.py
+++ grao.py
@@ -1,11 +1,10 @@
 import urllib.request
-
 import fuzzymatcher
 import pandas as pd
 import requests
 from io import BytesIO
 from zipfile import ZipFile
-from fuzzymatcher import link_table, fuzzy_left_join
+
 
 #url = "https://www.grao.bg/tna/tadr2020.txt"
 url = "https://www.grao.bg/tna/t41nm-15.12.2021_2.txt"
@@ -37,6 +36,8 @@ for line in data:
         a = a + sep + reg_str + decoded_line.lstrip()
     elif 'ГР.' in decoded_line:
         a = a + sep + reg_str + decoded_line.lstrip()
+    elif "МАН." in decoded_line:
+        a = a + sep + reg_str + decoded_line.lstrip()
     else:
         pass
 
@@ -47,6 +48,8 @@ for line in data:
 
 df = pd.DataFrame([x.split('|') for x in a.split('\r\n')])
 
+#check if raw data was properly saved
+#df.to_csv('df_check.csv', sep=',', encoding='utf-8', index=False)
 
 if len(df.columns) > 8:
     column_list = [0, 5, 6, 7, 9, 10, 11, 12]
@@ -58,21 +61,47 @@ else:
 
 df.columns = ['region', 'municipality', 'settlement', 'permanent_population', 'current_population']
 
-df['region'] = df['region'].str.lower()
-df['municipality'] = df['municipality'].str.lower()
-df['settlement'] = df['settlement'].str.lower()
-df['settlement'] = df['settlement'].str.replace('гр.', '', regex=False)
-df['settlement'] = df['settlement'].str.replace('с.', '', regex=False)
+# a lot of repeating code to turn into 2-3 functions
+columns_to_transform = ['region', 'municipality', 'settlement']
+for column in columns_to_transform:
+    df[column] = df[column].str.lower()
+
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('гр.', '', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('с.', '', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('ман.', '', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('ь', 'ъ', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('ъо', 'ьо', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('добричка', 'добрич', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('добрич-селска', 'добрич', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('добрич-град', 'добрич', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('софийска', 'софия', regex=False)
+
+for column in columns_to_transform:
+    df[column] = df[column].str.replace('столична', 'софия', regex=False)
+
 df['settlement'] = df['settlement'].str.strip()
 
-df['settlement'] = df['settlement'].str.replace('ь', 'ъ', regex=False)
-df['settlement'] = df['settlement'].str.replace('ъо', 'ьо', regex=False)
-df['municipality'] = df['municipality'].str.replace('ь', 'ъ', regex=False)
-df['municipality'] = df['municipality'].str.replace('ъо', 'ьо', regex=False)
-df['region'] = df['region'].str.replace('ь', 'ъ', regex=False)
-df['region'] = df['region'].str.replace('ъо', 'ьо', regex=False)
+#combined columns
+df['combined_column'] = df['region'] + '_' + df['municipality'] + '_' + df['settlement']
 
-df['combined_column'] = df['municipality'] + '_' + df['settlement']
 
 #TO DO SAVE DATAFRAME TO CSV WITH DATE AS FILENAME
 
@@ -91,7 +120,7 @@ with first_zip as z:
             df_ek_obst = pd.read_excel(second_zip.open('Ek_obst.xlsx'), converters={'ekatte': str})
 
 
-#EKATTE DATAFRAME
+# EKATTE DATAFRAME
 df_ekatte['name'] = df_ekatte['name'].str.lower()
 df_ekatte = df_ekatte.iloc[1: , :]
 columns_list_ek = ['t_v_m', 'kmetstvo', 'kind', 'category', 'altitude', 'document', 'tsb', 'abc']
@@ -101,7 +130,7 @@ df_ekatte.drop(columns_list_ek,
 df_ekatte = df_ekatte.rename({'ekatte': 'ekatte', 'name': 'settlement', 'oblast': 'region_code', 'obstina': 'mun_code'}, axis=1)
 
 
-#REGION DATAFRAME
+# REGION DATAFRAME
 df_ek_obl['name'] = df_ek_obl['name'].str.lower()
 columns_list_obl = ['document', 'abc', 'region', 'ekatte']
 df_ek_obl.drop(columns_list_obl,
@@ -110,7 +139,7 @@ df_ek_obl.drop(columns_list_obl,
 df_ek_obl = df_ek_obl.rename({'oblast': 'region_code', 'name': 'region'}, axis=1)
 
 
-#MUNICIPALITY DATAFRAME
+# MUNICIPALITY DATAFRAME
 df_ek_obst['name'] = df_ek_obst['name'].str.lower()
 columns_list_obst = ['ekatte', 'category', 'document', 'abc']
 df_ek_obst.drop(columns_list_obst,
@@ -118,39 +147,45 @@ df_ek_obst.drop(columns_list_obst,
         inplace=True)
 df_ek_obst = df_ek_obst.rename({'obstina': 'mun_code', 'name': 'municipality'}, axis=1)
 
-#print(df.head(10))
-# print(df_ekatte.head(10))
-# print(df_ek_obl.head(10))
-# print(df_ek_obst.head(10))
-
+# MERGED DATAFRAME WITH EKATTE CODES AND SETTLEMENT, REGION, MUNICIPALITY
 df_ekatte = pd.merge(df_ekatte, df_ek_obl,
                          how='left')
 df_ekatte = pd.merge(df_ekatte, df_ek_obst, how='left')
 df_ekatte = df_ekatte[['ekatte', 'region', 'municipality', 'settlement', 'region_code', 'mun_code']]
-df_ekatte['combined_column'] = df_ekatte['municipality'] + '_' + df_ekatte['settlement']
 
-# df = pd.merge(df, df_ek_obl[['name', 'oblast']], how='left', left_on = 'region', right_on = 'name').drop(columns= ['name'])
-# df = pd.merge(df, df_ek_obst[['name', 'obstina']], how='left', left_on = 'municipality', right_on = 'name').drop(columns= ['name'])
-#
-# df = pd.merge(df, df_ekate[['ekatte','name', 'oblast', 'obstina']], how='left', left_on = ['settlement','oblast','obstina'], right_on = ['name','oblast','obstina']).drop(columns= ['name'])
+
+for column in columns_to_transform:
+    df_ekatte[column] = df_ekatte[column].str.replace('добрич-селска', 'добрич', regex=False)
+
+for column in columns_to_transform:
+    df_ekatte[column] = df_ekatte[column].str.replace('софийска', 'софия', regex=False)
+
+for column in columns_to_transform:
+    df_ekatte[column] = df_ekatte[column].str.replace('столична', 'софия', regex=False)
+
+for column in columns_to_transform:
+    df_ekatte[column] = df_ekatte[column].str.replace('софия (столица)', 'софия', regex=False)
+
+# combined columns
+df_ekatte['combined_column'] = df_ekatte['region'] + '_' + df_ekatte['municipality'] + '_' + df_ekatte['settlement']
+
+# merge main df and ekatte to combine the matches and avoid mistakes in the next merges
 df4 = pd.merge(df, df_ekatte, how='left')
+
 #df4['combined_column'] = df4['region'] + '_' + df4['municipality'] + '_' + df4['settlement']
 
-#Fuzzy matching between df_ekatte and main df
+# Fuzzy matching between df_ekatte and df4 so as to avoid mismatching the "duplicate" values
 
-left_on = ['combined_column']
+left_on = ['ekatte', 'combined_column']
 
-# Columns to match on from df_right
-right_on = ['combined_column']
+right_on = ['ekatte', 'combined_column']
 
-# Matching the previously 1:1 matched initial dataframe with the ekatte dataframe, and then fuzzy matching to ekatte one, again to fill gaps
-matched_results = fuzzymatcher.fuzzy_left_join(df, df_ekatte, left_on, right_on)
+# Matching the previously 1:1 matched initial dataframe with the ekatte dataframe,
+# and then fuzzy matching to ekatte one, again to fill gaps
+matched_results = fuzzymatcher.fuzzy_left_join(df4, df_ekatte, left_on, right_on, right_id_col= 'ekatte')
 
-
-#matched_results = matched_results[['region_right', 'municipality_right', 'settlement_right', 'permanent_population', 'current_population', 'ekatte', 'region_code', 'mun_code']]
 matched_results.to_csv('matched_results.csv', sep=',', encoding='utf-8', index=False)
-df4.to_csv('df4.csv', sep=',', encoding='utf-8', index=False)
 df_ekatte.to_csv('df_ekatte.csv', sep=',', encoding='utf-8', index=False)
-#print(df)
+
 
 
