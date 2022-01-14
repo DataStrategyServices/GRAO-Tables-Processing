13906d6a6c91cb9ceb1ba9a75a1364fa7324611c
Dimitar <dimitar.atanasov@indeavr.com>
2022-01-14 16:55:27 +0200

    More or less done, need to test with most recent data after historic is done

--- grao_table_processing/grao.py
+++ grao_table_processing/grao.py
@@ -9,25 +9,56 @@ from grao_table_processing.wikidata_codes import WikidataCodes
 from grao_table_processing.wikidata_uploader import upload_to_wikidata
 
 
-logging.basicConfig(filename='logs/ekatte.log', filemode='a+', level=logging.WARNING,
+logging.basicConfig(filename='logs/ekatte.log', filemode='a+', level=logging.CRITICAL,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+"""
+This is the project's main file, it has a function for each of the main classes, as well
+as the wikidata uploader file. The imports are above, as well as split into each file.
+There is a simple logger to track any critical errors.
+"""
 
 
 def generate_url(url_object: DataURL()) -> str:
+    """
+    Calls the DataURL class and provides a URL to extract data from
+
+    Args:
+        url_object: DataURL() class object
+    Return:
+        DataURL.generate_data_url(url_object): string
+    """
     return DataURL.generate_data_url(url_object)
 
 
-# ''''''''''''''' Create markdown object '''''''''''''''''
 def extract_date(url: str) -> datetime.date:
+    """
+    By using the result of the generate URL function, extracts the date to be used
+    in the data loading.
+
+    Args:
+        url: a string URL that leads to a markdown table for date extraction
+    Return:
+        date_object: datetime.date object
+    """
     markdown_object = ReadMarkdownTable(url, '|')
     date_object = markdown_object.extract_date()
     return date_object
 
 
 def transformations(url: str) -> pd.DataFrame:
+    """
+    This function invokes the ReadMarkdownTable class, and does all necessary
+    transformations so that it is ready to be merged forward.
+
+    Args:
+        url: a string URL that leads to a markdown table for data extraction
+    Return:
+        markdown_object.markdown_df: pd.DataFrame
+    """
     markdown_object = ReadMarkdownTable(url, '|')
 
-# ''''''''''''''' Drop unused column '''''''''''''''''
+# ''''''''''''''' Drop unused columns '''''''''''''''''
     column_list = [0, 5, 6, 7, 9, 10, 11, 12]
     markdown_object.drop_attributes(column_list)
 
@@ -60,27 +91,72 @@ def transformations(url: str) -> pd.DataFrame:
 
 
 def merge_with_ekatte(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    The function invokes the EkatteDataframe class, produces the ekatte_dataframe
+    and merges it with the GRAO markdown table.
+
+    Args:
+        dataframe: pd.DataFrame
+    Return:
+        EkatteDataframe.merge_with_main(ekatte_frame): a pd.DataFrame
+    """
     ekatte_frame = EkatteDataframe(dataframe)
     return EkatteDataframe.merge_with_main(ekatte_frame)
 
 
 def merge_with_q_codes(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    The function invokes the WikidataCodes class, produces the Q code dataframe
+    and merges it with the GRAO Dataframe with Ekatte codes.
+
+    Args:
+        dataframe: pd.DataFrame
+    Return:
+        matched_data: pd.DataFrame, ready to have its values uploaded to WikiData
+        """
     wikidata_codes = WikidataCodes(dataframe)
     matched_data = WikidataCodes.merge_with_ekatte(wikidata_codes)
     return matched_data
 
 
-#
 def upload_data(matched_data: pd.DataFrame, url: str, date: datetime.date) -> None:
+    """
+    This function invokes the Upload_to_wikidata function from the wikidata_uploader module.
+    it carries out the functions by clearing out the ranks, uploading the data quarter by quarter
+    for all settlements.
+
+    Args:
+        matched_data: pd.DataFrame
+        url: string
+        date: datetime.date
+    Return:
+        None
+    """
     upload_to_wikidata(matched_data, url, date)
-#
 
 
 def update_date_file(url_object: DataURL()) -> None:
+    """
+    If the upload concludes successfully, this function is invoked. It finally updates
+    the date helper file with the most recent date, so that the future uploads happen on schedule.
+    The URL is added to the processed reports log for archiving.
+
+    Args:
+        url_object: DataURL() class object
+    Return:
+        None
+    """
     DataURL.update_date_file(url_object)
 
 
 def main():
+    """
+    The main function sets the parameters for every function in the grao.py module.
+    It carries out the entire logic of the project from data extraction to loading.
+
+    Return:
+        None
+    """
     url_object = DataURL()
     url = generate_url(url_object)
     date = extract_date(url)
